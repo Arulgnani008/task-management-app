89162c7110b123af6e8d09eb887c3a818f723787
sangeerthan756-commits <sangeerthan756@gmail.com>
2026-06-10 19:10:21 +0530

    Initial Task Management App

---
+++ public/script.js
@@ -0,0 +1,18 @@
+function addTask() {
+
+    let input = document.getElementById("taskInput");
+
+    if(input.value === ""){
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML =
+        input.value +
+        ' <button onclick="this.parentElement.remove()">Delete</button>';
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
